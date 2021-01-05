f8b1e5f582c26a3c841c336447f3a1b3e8fc1086
pootz10 <pootz10@gmail.com>
2021-01-05 18:15:51 -0300

    Update Instagram_Image_Link_Opener.user.js
    // @run-at      document-end

--- Instagram_Image_Link_Opener.user.js
+++ Instagram_Image_Link_Opener.user.js
@@ -6,14 +6,16 @@
 // @include     http://www.instagram.com/*
 // @include     https://instagram.com/*
 // @include     http://instagram.com/*
-// @version     1.1
+// @version     1.2
+// @history     v1.2 when it has multiple images on a post and the tab is not active script may not find the image link
 // @history     v1.1 Update IG source code
 // @license     MIT
 // @require     https://code.jquery.com/jquery-2.2.3.min.js
 // @require     https://gist.githubusercontent.com/BrockA/2625891/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
-// @updateURL   https://github.com/pootz10/Instagram-Image-Link-Opener/raw/master/Instagram_Image_Link_Opener.user.js
-// @downloadURL https://github.com/pootz10/Instagram-Image-Link-Opener/raw/master/Instagram_Image_Link_Opener.user.js
+// @updateURL   https://raw.githubusercontent.com/pootz10/Instagram-Image-Link-Opener/master/Instagram_Image_Link_Opener.user.js
+// @downloadURL https://raw.githubusercontent.com/pootz10/Instagram-Image-Link-Opener/master/Instagram_Image_Link_Opener.user.js
 // @grant       GM_openInTab
+// @run-at      document-end
 // ==/UserScript==
 
 waitForKeyElements("div.KL4Bh img", clickableImage, false);
@@ -30,7 +32,6 @@ function clickableImage (jNode) {
              GM_openInTab(imgLink);
 
          });
-
 
      } );
 
